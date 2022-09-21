4100686bd9d716d56e59c99f07e62ef086ed3572
Debian <admin@ip-172-82-11-159.ec2.internal>
2022-09-21 07:09:44 +0000

    First push

---
+++ src/main/java/com/github/luyomo/kafka/connect/transform/TiDB.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright ©20229Jay(syunka.tyo@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.luyomo.kafka.connect.tidb;
+
+import org.apache.kafka.common.cache.Cache;
+import org.apache.kafka.common.cache.LRUCache;
+import org.apache.kafka.common.cache.SynchronizedCache;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+
+import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SchemaUtil;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
+import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
+
+public abstract class TiDB<R extends ConnectRecord<R>> implements Transformation<R> {
+  private static final Logger logger = LoggerFactory.getLogger(TiDB.class);
+
+  public static final String OVERVIEW_DOC =
+    "Insert a random UUID into a connect record";
+
+  private interface ConfigName {
+    String UUID_FIELD_NAME = "uuid.field.name";
+  }
+
+  public static final ConfigDef CONFIG_DEF = new ConfigDef()
+    .define(ConfigName.UUID_FIELD_NAME, ConfigDef.Type.STRING, "uuid", ConfigDef.Importance.HIGH,
+      "Field name for UUID");
+
+  private static final String PURPOSE = "adding UUID to record";
+
+  private String fieldName;
+
+  private Cache<Schema, Schema> schemaUpdateCache;
+
+  @Override
+  public void configure(Map<String, ?> props) {
+    logger.info("********** 02. into the configure function");
+    final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+    fieldName = config.getString(ConfigName.UUID_FIELD_NAME);
+
+    schemaUpdateCache = new SynchronizedCache<>(new LRUCache<Schema, Schema>(16));
+  }
+
+
+  @Override
+  public R apply(R record) {
+//    logger.info("********** 02. This is the test message for logging <{}>", record.toString());
+    if (operatingSchema(record) == null) {
+      return applySchemaless(record);
+    } else {
+      return applyWithSchema(record);
+    }
+  }
+
+  private R applySchemaless(R record) {
+    final Map<String, Object> value = requireMap(operatingValue(record), PURPOSE);
+
+    final Map<String, Object> updatedValue = new HashMap<>(value);
+
+    updatedValue.put(fieldName, getRandomUuid());
+
+    return newRecord(record, null, updatedValue);
+  }
+
+  private R applyWithSchema(R record) {
+    final Struct value = requireStruct(operatingValue(record), PURPOSE);
+    logger.info("********** 02. The schema from applyWithSchema <{}>", value.schema().toString());
+    logger.info("***** 001. The value is {}", value);
+
+    Schema updatedSchema = schemaUpdateCache.get(value.schema());
+    logger.info("****** 002.  The schema 0001 <{}>", updatedSchema);
+    if(updatedSchema == null) {
+      logger.info("002.01 The schema here is {}", value.schema()); 
+      updatedSchema = makeUpdatedSchema(value.schema());
+      logger.info("***** 003. The schema before add: <{}>", updatedSchema.toString());
+      schemaUpdateCache.put(value.schema(), updatedSchema);
+      logger.info("***** 004. The schema into cache add: <{}>", schemaUpdateCache.toString());
+    }
+
+    logger.info("********** 005. The data before add value <{}>", updatedSchema.toString());
+    final Struct updatedValue = new Struct(updatedSchema);
+
+    for (Field field : value.schema().fields()) {
+      logger.info("***** 006. The field name is <{}>", field.name());
+      if(field.name().equals("t_bit"))  {
+          logger.info("Setting the data as bit");
+          updatedValue.put(field.name(), true);
+          logger.info("Reached the data set");
+      }else{
+          updatedValue.put(field.name(), value.get(field));
+      }
+
+    }
+
+    logger.info("********** 008. The data before add value <{}>", updatedValue.toString());
+    updatedValue.put(fieldName, getRandomUuid());
+    logger.info("********** 02. This is the test message after data update <{}>", updatedValue.toString());
+
+    return newRecord(record, updatedSchema, updatedValue);
+  }
+
+  @Override
+  public ConfigDef config() { 
+    logger.info("********** 02. into the config function");
+    return CONFIG_DEF;
+  }
+
+  @Override
+  public void close() {
+    schemaUpdateCache = null;
+  }
+
+  private String getRandomUuid() {
+    return UUID.randomUUID().toString();
+  }
+
+  private Schema makeUpdatedSchema(Schema schema) {
+    final SchemaBuilder builder = SchemaUtil.copySchemaBasics(schema, SchemaBuilder.struct());
+
+    for (Field field: schema.fields()) {
+      logger.info("The field name is: <{}>, schema is: <{}>", field.name(), field.schema());
+      if (field.name().equals("t_bit") ) {
+          builder.field(field.name(), Schema.BOOLEAN_SCHEMA);
+      } else {
+          builder.field(field.name(), field.schema());
+      }
+    }
+
+    builder.field(fieldName, Schema.STRING_SCHEMA);
+
+    return builder.build();
+  }
+
+  protected abstract Schema operatingSchema(R record);
+
+  protected abstract Object operatingValue(R record);
+
+  protected abstract R newRecord(R record, Schema updatedSchema, Object updatedValue);
+
+  public static class Key<R extends ConnectRecord<R>> extends TiDB<R> {
+
+    @Override
+    protected Schema operatingSchema(R record) {
+      return record.keySchema();
+    }
+
+    @Override
+    protected Object operatingValue(R record) {
+      return record.key();
+    }
+
+    @Override
+    protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+      return record.newRecord(record.topic(), record.kafkaPartition(), updatedSchema, updatedValue, record.valueSchema(), record.value(), record.timestamp());
+    }
+
+  }
+
+  public static class Value<R extends ConnectRecord<R>> extends TiDB<R> {
+
+    @Override
+    protected Schema operatingSchema(R record) {
+      return record.valueSchema();
+    }
+
+    @Override
+    protected Object operatingValue(R record) {
+      return record.value();
+    }
+
+    @Override
+    protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+      return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), updatedSchema, updatedValue, record.timestamp());
+    }
+
+  }
+}
+
+
---
+++ src/test/java/com/github/luyomo/kafka/connect/tidb/TiDB.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright ©20229Jay (syunka.tyo@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.luyomo.kafka.connect.tidb;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TiDBTest {
+
+  private TiDB<SourceRecord> xform = new TiDB.Value<>();
+
+  @After
+  public void tearDown() throws Exception {
+    xform.close();
+  }
+
+  @Test(expected = DataException.class)
+  public void topLevelStructRequired() {
+    xform.configure(Collections.singletonMap("uuid.field.name", "myUuid"));
+    xform.apply(new SourceRecord(null, null, "", 0, Schema.INT32_SCHEMA, 42));
+  }
+
+  @Test
+  public void copySchemaAndTiDBField() {
+    final Map<String, Object> props = new HashMap<>();
+    System.out.println("\n\n\n\nHello world in the copySchemaAndTiDBField \n\n\n");
+
+    props.put("uuid.field.name", "myUuid");
+
+    xform.configure(props);
+
+    final Schema simpleStructSchema = SchemaBuilder.struct().name("name").version(1).doc("doc").field("magic", Schema.OPTIONAL_INT64_SCHEMA).build();
+    System.out.printf("\n Schema name %s \n ", simpleStructSchema.name());
+    System.out.printf(" Doc:        %s \n ", simpleStructSchema.doc());
+    System.out.printf(" Version:    %s \n ", simpleStructSchema.version());
+    System.out.printf(" Fields:     %s \n ", simpleStructSchema.fields());
+    System.out.printf(" One Field:  %s \n ", simpleStructSchema.field("magic").schema());
+    final Struct simpleStruct = new Struct(simpleStructSchema).put("magic", 42L);
+    System.out.printf(" The data is : %s \n", simpleStruct.toString());
+
+    final SourceRecord record = new SourceRecord(null, null, "test", 0, simpleStructSchema, simpleStruct);
+    
+    final SourceRecord transformedRecord = xform.apply(record);
+
+    assertEquals(simpleStructSchema.name(), transformedRecord.valueSchema().name());
+    assertEquals(simpleStructSchema.version(), transformedRecord.valueSchema().version());
+    assertEquals(simpleStructSchema.doc(), transformedRecord.valueSchema().doc());
+
+    assertEquals(Schema.OPTIONAL_INT64_SCHEMA, transformedRecord.valueSchema().field("magic").schema());
+    assertEquals(42L, ((Struct) transformedRecord.value()).getInt64("magic").longValue());
+    assertEquals(Schema.STRING_SCHEMA, transformedRecord.valueSchema().field("myUuid").schema());
+    assertNotNull(((Struct) transformedRecord.value()).getString("myUuid"));
+
+    // Exercise caching
+    final SourceRecord transformedRecord2 = xform.apply(
+      new SourceRecord(null, null, "test", 1, simpleStructSchema, new Struct(simpleStructSchema)));
+    assertSame(transformedRecord.valueSchema(), transformedRecord2.valueSchema());
+
+  }
+
+  @Test
+  public void schemalessTiDBField() {
+    final Map<String, Object> props = new HashMap<>();
+
+    props.put("uuid.field.name", "myUuid");
+
+    xform.configure(props);
+
+    final SourceRecord record = new SourceRecord(null, null, "test", 0,
+      null, Collections.singletonMap("magic", 42L));
+
+    final SourceRecord transformedRecord = xform.apply(record);
+    assertEquals(42L, ((Map) transformedRecord.value()).get("magic"));
+    assertNotNull(((Map) transformedRecord.value()).get("myUuid"));
+
+  }
+}
